e47f1acf03c475887ff159140dbfdb6aa7055f3f
lugal-cn <lugal.cn@gmail.com>
2014-04-01 21:11:34 +0800

    .gitignore is now working

--- data/sysdata/cache_pluginsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: d153e8b2b1b3166b8b37f72dc8be0c46
-
-$pluginsetting = array (
-);
-?>
--- source/admincp/admincp_login.php
+++ source/admincp/admincp_login.php
@@ -87,8 +87,10 @@ function html_login_footer($halt = true) {
 <tr>
 	<td colspan="2" class="footer">
 		<div class="copyright">
+			<!--
 			<p>Powered by <a href="http://www.discuz.net/" target="_blank">Discuz!</a> $version </p>
 			<p>&copy; 2001-2013, <a href="http://www.comsenz.com/" target="_blank">Comsenz</a> Inc.</p>
+			-->
 		</div>
 	</td>
 </tr>
